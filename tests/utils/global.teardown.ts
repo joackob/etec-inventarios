7cdbce643aad10bc47e771f059bf38f0d8eb4d44
Rodrigo Villca <villcarodrigo70@gmail.com>
2024-10-29 19:51:23 -0300

    new: ahora se eliminan los datos de los test de la base de datos

--- tests/utils/global.teardown.ts
+++ tests/utils/global.teardown.ts
@@ -2,6 +2,10 @@ import db from "@/db";
 
 export const limpiarBaseDeDatos = async () => {
   await eliminarTodosLosUsuarios();
+  await eliminarTodasLasSolicitudes();
+  await eliminarTodosLosItems();
+  await eliminarTodasLasUbicaciones();
+  await eliminarLosItemsEnSolicitud();
 };
 
 const eliminarTodosLosUsuarios = async () => {
@@ -11,5 +15,36 @@ const eliminarTodosLosUsuarios = async () => {
     console.log("no se pudieron eliminar los usuarios");
   }
 };
+
+const eliminarTodasLasSolicitudes = async () => {
+  try {
+    await db.solicitudes.deleteMany();
+  } catch (error) {
+    console.log("no se pudieron eliminar las solicitudes");
+  }
+};
+
+const eliminarTodosLosItems = async () => {
+  try {
+    await db.items.deleteMany();
+  } catch (error) {
+    console.log("no se pudieron eliminar los items");
+  }
+};
+const eliminarTodasLasUbicaciones = async () => {
+  try {
+    await db.ubicaciones.deleteMany();
+  } catch (error) {
+    console.log("no se pudieron eliminar las ubicaciones");
+  }
+};
+const eliminarLosItemsEnSolicitud = async () => {
+  try {
+    await db.itemsEnSolicitud.deleteMany();
+  } catch (error) {
+    console.log("no se pudieron eliminar las ubicaciones");
+  }
+};
+
 
 export default limpiarBaseDeDatos;
