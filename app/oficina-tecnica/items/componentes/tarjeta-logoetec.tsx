13c7b797c3a697cdd1e91b14ebb2bea19e7d3cbb
Brandon Parra <tuberculosis1287@gmail.com>
2024-11-22 18:02:03 -0300

    New: Nueva pagina de items

---
+++ app/oficina-tecnica/items/componentes/tarjeta-logoetec.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Paper, Stack } from "@mui/material";
+import { LogoETEC } from "@/app/components/logo-etec";
+
+export default function TarjetaParaLogoEtec() {
+    return (
+
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+            <Paper style={{ backgroundColor: "FFFFFF" }}>
+                <Stack
+                    bgcolor={"#FAFDFD"}
+                    borderRadius={2}
+                    direction={"row"}
+                    spacing={"16px"}
+                    padding={"16px"}
+                    alignItems={"center"}
+                >
+                    {/* <img src="" alt="" /> */}
+                </Stack>
+                <LogoETEC></LogoETEC>
+            </Paper>
+        </div>
+    );
+}
---
+++ app/oficina-tecnica/items/page.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import db from "@/db";
+import TarjetaParaLogoEtec from "./componentes/tarjeta-logoetec";
+
+
+export default async function Page({}) {
+    return(
+        <TarjetaParaLogoEtec/>
+
+    )
+  }
+
+  
